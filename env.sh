d12bc1486c739ef30b2bb30aa80b7b9defc4c2fa
Chintan Patel <chintanrp@github.com>
2020-03-16 11:38:52 -0700

    Add comment for default AppInsights Endpoint environment url

--- env.sh
+++ env.sh
@@ -21,4 +21,6 @@ CREDSCANSERVICEVOLUMENAME="CredScanSvc"
 
 # TODO: replace this with envionment specific AppInsights instrumentation key
 APPINSIGHTS_KEY='00000000-0000-0000-00000000000000000'
+# Replace this with environment specific AppInsights Endpoint. Below is the defualt for Azure Public environemnt.
+# Will be relace by environment variable for other clouds while service deployment
 APPINSIGHTS_ENDPOINTURL='https://dc.services.visualstudio.com/v2/track'
